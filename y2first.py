6751555018c238dcfeb037c3f97da3e5c6a07add
NuranGh24-meet <109899316+NuranGh23-meet@users.noreply.github.com>
2022-07-24 13:33:43 +0300

    Create y2first.py

---
+++ y2first.py
@@ -0,0 +1,8 @@
+print("hello world")
+print ("I once had seven cats")
+print("plays soccer")
+print("favorite color is purple")
+#in terminal
+#we begin with ls which is list files then all of the files are called
+#after the files are recalled we write cd to specify the folder on which location such as the folder or file location is in deskktop we write cd Desktop 
+#and the cd.. is to return a step from what youve done which returns into the step before the step you made before
